2b2e31c4bc2c5bd118db60f896299cd00ef5bf15
victormolgus <56732109+victormolgus@users.noreply.github.com>
2021-06-05 22:43:46 -0300

    Add files via upload

---
+++ Oficina1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String n1, n2;
+            Console.WriteLine("|----------------------|");
+            Console.WriteLine("| Cálculo da Diferença |");
+            Console.WriteLine("|----------------------|\n");
+            //Recebo os valores n1 e n2
+            Console.Write(" Digite o primeiro número: ");
+            n1 = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write(" Digite o segundo número: ");
+            n2 = Console.ReadLine();
+            //resultado recebe a diferença da conversão dos valores para inteiro.
+            double resultado;
+            resultado = int.Parse(n1) - int.Parse(n2);
+            Console.WriteLine("|--------------");
+            Console.WriteLine("| Resultado: " + resultado);
+            Console.WriteLine("|--------------");
+            //Espero a ação do usuário.
+            Console.Write(" Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Oficina1.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Oficina1.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Oficina1.1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ba309db1-35c9-47c4-baa3-3a8b4e4e977d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
