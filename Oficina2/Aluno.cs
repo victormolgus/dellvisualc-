31a0e746823b41df8291fb5696159bc64f6b903a
victormolgus <56732109+victormolgus@users.noreply.github.com>
2021-07-07 01:29:49 -0300

    Add files via upload

---
+++ Oficina2/Aluno.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina2
+{
+    public class Aluno
+    {
+        //Coloco os atributos em privado
+        private String nome_aluno;
+        private int mat;
+        private String email;
+
+        //Defino o construtor
+        public Aluno(string nome_aluno, int mat, string email)
+        {
+            this.nome_aluno = nome_aluno;
+            this.mat = mat;
+            this.email = email;
+        }
+        //Defino os setters e getters
+        public void setNome_Aluno(String nome_aluno)
+        {
+            this.nome_aluno = nome_aluno;
+        }
+
+        public String getNome_Aluno()
+        {
+            return this.nome_aluno;
+        }
+
+        public void setMat(int mat)
+        {
+            this.mat = mat;
+        }
+
+        public int getMat()
+        {
+            return this.mat;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public String getEmail()
+        {
+            return this.email;
+        }
+
+
+        //Defino os métodos
+        public void inserirDisciplina()
+        {
+            Console.WriteLine("Insira o nome da disciplina");
+            Console.WriteLine("O aluno " + this.nome_aluno + " inseriu a disciplina de " + Console.ReadLine());
+
+        }
+        public void removerDisciplina()
+        {
+            Console.WriteLine("Insira o nome da disciplina");
+            Console.WriteLine("O aluno " + this.nome_aluno + " removeu a disciplina de " + Console.ReadLine());
+
+        }
+        public void realizarMatricula()
+        {
+            Console.WriteLine("O aluno " + this.nome_aluno + " foi matriculado ");
+            Console.WriteLine("Matrícula: " + this.mat);
+
+        }
+    }
+}
---
+++ Oficina2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Oficina2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Defino quatro objetos, dois para instanciar a classe aluno e dois para professor
+            Aluno a1 = new Aluno("Matt Murdock", 12345, "daredevil@gmail.com");
+            Aluno a2 = new Aluno("Foggy Nelson", 67890, "nelson@gmail.com");
+            Professor p1 = new Professor("Charles Xavier", 7987987, 2500);
+            Professor p2 = new Professor("Otto Octavius", 1253456, 2700);
+
+            //Aqui exibo os dados dos dois alunos
+            Console.WriteLine("Aluno 1");
+            Console.WriteLine("--------");
+            Console.WriteLine(a1.getNome_Aluno());
+            Console.WriteLine(a1.getEmail());
+            Console.WriteLine(a1.getMat());
+            Console.WriteLine();
+
+            Console.WriteLine("Aluno 2");
+            Console.WriteLine("--------");
+            Console.WriteLine(a2.getNome_Aluno());
+            Console.WriteLine(a2.getEmail());
+            Console.WriteLine(a2.getMat());
+            Console.WriteLine();
+
+            //Aqui exibo os dados dos dois professores
+            Console.WriteLine("Professor 1");
+            Console.WriteLine("--------");
+            Console.WriteLine(p1.getNome_Professor());
+            Console.WriteLine(p1.getNum_Carteira());
+            Console.WriteLine(p1.getSalario());
+            Console.WriteLine();
+
+            Console.WriteLine("Professor 2");
+            Console.WriteLine("--------");
+            Console.WriteLine(p2.getNome_Professor());
+            Console.WriteLine(p2.getNum_Carteira());
+            Console.WriteLine(p2.getSalario());
+            Console.WriteLine();
+
+
+            //Aqui exibo os métodos da classe aluno
+            a1.inserirDisciplina();
+            Console.WriteLine();
+            a2.removerDisciplina();
+            Console.WriteLine();
+            a1.realizarMatricula();
+            Console.WriteLine();
+
+            //a2.inserirDisciplina();
+            //a1.removerDisciplina();
+            //a2.realizarMatricula();
+
+            //Aqui exibo os métodos da classe professor
+            p1.postarConteudo();
+            Console.WriteLine();
+            p1.realizarChamada();
+            Console.WriteLine();
+
+            p2.postarConteudo();
+            Console.WriteLine();
+            p2.realizarChamada();
+            Console.WriteLine();
+
+            //Uso para que o programa se encerre após o clique de qualquer tecla
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
