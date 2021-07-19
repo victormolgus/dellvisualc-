76e3ca36d413f6df9dcdd11c68d524676ce44456
victormolgus <56732109+victormolgus@users.noreply.github.com>
2021-07-19 16:26:52 -0300

    Add files via upload

---
+++ VictorSantos_Oficina4.1/Oficina4.1/Oficina4.1/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Oficina4._1
+{
+    [Table("Cliente")]
+    class Cliente
+    {
+        [Key]
+        public int cod_Cliente { get; set; }
+        public String nome_cliente { get; set; }
+        public String endereco { get; set; }
+    }
+}
---
+++ VictorSantos_Oficina4.1/Oficina4.1/Oficina4.1/ClienteContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Oficina4._1
+{
+    class ClienteContext:DbContext
+    {
+        public ClienteContext():base("Oficina4.1")
+        { }
+        public DbSet<Cliente> Cliente { get; set; }
+
+
+    }
+}
