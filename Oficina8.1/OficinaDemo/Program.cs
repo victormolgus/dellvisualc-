175f71a7891e41303100493d2cfc011b621dcb08
victormolgus <56732109+victormolgus@users.noreply.github.com>
2021-07-31 00:56:07 -0300

    Add files via upload

---
+++ Oficina8.1/OficinaDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string amigo = "Amigo";
+            Console.WriteLine("Olá " + amigo);
+        }
+    }
+}
